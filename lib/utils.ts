f76ebb2a1b9436f13e07cde88271a6d98822f7fa
mike foley <mikefoley@mikes-MacBook-Pro.local>
2025-11-15 09:01:57 -0500

    Add missing shadcn utils file

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+// Utility function used throughout shadcn/ui components for merging class names.
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs));
+}
+
